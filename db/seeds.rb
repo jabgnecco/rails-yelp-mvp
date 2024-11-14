5e44e0f5dfbd68b87564c6139373c0f6ad66ec61
Jaime Barros <jabgnecco@gmail.com>
2024-11-14 11:19:51 +0000

    Yeld MVP Challenge

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Clean the database 🗑️
+puts "Cleaning Database..."
+Restaurant.destroy_all
+
+10.times do (
+  restaurant = Restaurant.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number ,
+    category: "chinese")
+  )
+  2.times do (
+  Review.create!(
+    rating: 3,
+    content: Faker::Restaurant.review,
+    restaurant: restaurant)
+  )
+  end
+  puts "Restaurant Created..."
+end
